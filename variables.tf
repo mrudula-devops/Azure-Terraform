0ab63719bd4e34e2c509868d900c334c4092097c
mrudula-devops <anchamrudula@gmail.com>
2025-04-03 22:23:03 +0530

    sysntax

--- variables.tf
+++ variables.tf
@@ -1,27 +1,24 @@
 variable "azure_subscription_id" {
   description = "Azure Subscription ID"
   type        = string
-  default = "AZURE_SUBSCRIPTION_ID"
-  
+  default     = "AZURE_SUBSCRIPTION_ID"
 }
 
 variable "azure_client_id" {
   description = "Azure Client ID"
   type        = string
-  default = "AZURE_CLIENT_ID"
-  
+  default     = "AZURE_CLIENT_ID"
 }
 
 variable "azure_client_secret" {
   description = "Azure Client Secret"
   type        = string
   sensitive   = true
-  default = "AZURE_CLIENT_SECRET"
-  
+  default     = "AZURE_CLIENT_SECRET"
 }
 
 variable "azure_tenant_id" {
   description = "Azure Tenant ID"
   type        = string
-  default = "AZURE_TENANT_ID"
+  default     = "AZURE_TENANT_ID"
 }

b93fd49756ed46c33c247231f20d8db3bb56ed88
mrudula-devops <anchamrudula@example.com>
2025-04-03 20:01:17 +0530

    Initial commit - Terraform AKS setup

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "my-aks-rg"
+  location = "eastus"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "my-aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "myaksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}

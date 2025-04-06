e22a6f95cb76116d202bb396f4f4a12a386a860d
mrudula-devops <anchamrudula@example.com>
2025-04-06 13:32:46 +0530

    Final last

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,24 @@
-variable "azure_subscription_id" {}
-variable "azure_client_id" {}
-variable "azure_client_secret" {}
-variable "azure_tenant_id" {}
+variable "azure_client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "azure_client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
 
 variable "resource_group_name" {
   default = "rg-aks-demo"
